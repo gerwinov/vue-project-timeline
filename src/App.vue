433d8e703d4b6197179fa25ffc4b3947daa6de31
Gerwin Overeem <gerwin@foryard.nl>
2018-08-26 19:01:07 +0200

    Variable width for timeline element

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <template>
   <div id="app">
-    <div class="flex flex-row flex-wrap">
+    <div class="flex">
       <div class="flex w-full sm:w-1/2 md:w-1/4 sm:justify-end pr-4" :style="{ backgroundColor: timelineBgColor }">
         <div class="flex flex-col border-r-2 py-4" :style="{ borderColor: timelineColor }">
-          <div class="flex text-white pr-2 h-10 items-center justify-end" v-for="year in timelineYears">
+          <div class="flex text-white pr-2 h-10 items-center" v-for="year in timelineYears">
             <h3 class="font-normal">{{ year }}</h3>
           </div>
         </div>
@@ -12,7 +12,7 @@
             <div class="h-10" v-for="n in getProjectSpacing(timeline, key)"></div>
             <div
               class="h-10 w-3 cursor-pointer"
-              :class="{ 'animated pulse shadow-md': selectedProject === projectId }"
+              :class="{ 'animated pulse shadow-md w-5': selectedProject === projectId }"
               :style="getTimelineElementStyle(projectId)"
               v-for="n in getProjectYears(projectId)"
               @click="selectedProject = projectId">
@@ -35,173 +35,173 @@
 </template>
 
 <script>
-import ProjectCard from './components/ProjectCard.vue'
+  import ProjectCard from './components/ProjectCard.vue'
 
-export default {
-  name: 'app',
+  export default {
+    name: 'app',
 
-  components: {
-    ProjectCard
-  },
-
-  props: {
-    timelineColor: {
-      required: false,
-      default: '#979797',
-      type: String
+    components: {
+      ProjectCard
     },
 
-    timelineBgColor: {
-      required: false,
-      default: '#99B0A8',
-      type: String
+    props: {
+      timelineColor: {
+        required: false,
+        default: '#979797',
+        type: String
+      },
+
+      timelineBgColor: {
+        required: false,
+        default: '#99B0A8',
+        type: String
+      },
+
+      selectedBgColor: {
+        required: false,
+        default: '#F7B93A',
+        type: String
+      },
+
+      hoverBgColor: {
+        required: false,
+        default: '#FFEDCB',
+        type: String
+      },
     },
 
-    selectedBgColor: {
-      required: false,
-      default: '#F7B93A',
-      type: String
-    },
-
-    hoverBgColor: {
-      required: false,
-      default: '#FFEDCB',
-      type: String
-    },
-  },
-
-  data () {
-    return {
-      projects: [
-        {
-          title: 'Ebicus',
-          subtitle: 'Consultant',
-          description: `Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-                        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-                        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-                        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-                        cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-                        proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`,
-          image: `https://orig00.deviantart.net/e475/f/2011/338/e/f/the_green_circle___flag___logo_by_exxp0-d4i51ve.png`,
-          startYear: 2010,
-          endYear: 2017,
-          color: 'blue'
-        },
-        {
-          title: 'Overeem.io',
-          subtitle: 'Frontend developer',
-          description: `Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-                        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-                        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-                        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-                        cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-                        proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`,
-          image: `https://orig00.deviantart.net/e475/f/2011/338/e/f/the_green_circle___flag___logo_by_exxp0-d4i51ve.png`,
-          startYear: 2018,
-          endYear: 2018,
-          color: 'yellow'
-        },
-        {
-          title: 'Foryard',
-          subtitle: 'Frontend developer',
-          description: `Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-                        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-                        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-                        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-                        cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-                        proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`,
-          image: `https://orig00.deviantart.net/e475/f/2011/338/e/f/the_green_circle___flag___logo_by_exxp0-d4i51ve.png`,
-          startYear: 2017,
-          endYear: 2018,
-          color: 'green'
-        }
-      ],
-      selectedProject: 0
-    }
-  },
-
-  computed: {
-    sortedProjects () {
-      return this.projects.slice().sort((a, b) => {
-        if (a.endYear > b.endYear) {
-          return -1;
-        }
-        if (a.endYear < b.endYear) {
-          return 1;
-        }
-        return 0;
-      })
-    },
-
-    endYear () {
-      return Math.max.apply(Math, this.projects.map((project) => {
-        return project.endYear
-      }))
-    },
-
-    startYear () {
-      return Math.min.apply(Math, this.projects.map((project) => {
-        return project.startYear
-      }))
-    },
-
-    timelines () {
-      let timelines = []
-
-      this.sortedProjects.forEach((project, index) => {
-        this.addToTimeline(project.endYear, index, timelines)
-      })
-
-      return timelines
-    },
-
-    timelineYears () {
-      let timelineYears = []
-
-      for (let i = this.endYear; i >= this.startYear; i--) {
-        timelineYears.push(i)
-        if (i !== this.startYear) timelineYears.push(null)
+    data () {
+      return {
+        projects: [
+          {
+            title: 'Ebicus',
+            subtitle: 'Consultant',
+            description: `Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                          tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                          quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                          consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                          cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                          proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`,
+            image: `https://orig00.deviantart.net/e475/f/2011/338/e/f/the_green_circle___flag___logo_by_exxp0-d4i51ve.png`,
+            startYear: 2010,
+            endYear: 2017,
+            color: 'blue'
+          },
+          {
+            title: 'Overeem.io',
+            subtitle: 'Frontend developer',
+            description: `Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                          tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                          quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                          consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                          cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                          proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`,
+            image: `https://orig00.deviantart.net/e475/f/2011/338/e/f/the_green_circle___flag___logo_by_exxp0-d4i51ve.png`,
+            startYear: 2018,
+            endYear: 2018,
+            color: 'yellow'
+          },
+          {
+            title: 'Foryard',
+            subtitle: 'Frontend developer',
+            description: `Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                          tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                          quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                          consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                          cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                          proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`,
+            image: `https://orig00.deviantart.net/e475/f/2011/338/e/f/the_green_circle___flag___logo_by_exxp0-d4i51ve.png`,
+            startYear: 2017,
+            endYear: 2018,
+            color: 'green'
+          }
+        ],
+        selectedProject: 0
       }
+    },
 
-      return timelineYears
-    }
-  },
+    computed: {
+      sortedProjects () {
+        return this.projects.slice().sort((a, b) => {
+          if (a.endYear > b.endYear) {
+            return -1;
+          }
+          if (a.endYear < b.endYear) {
+            return 1;
+          }
+          return 0;
+        })
+      },
 
-  methods: {
-    addToTimeline (projectEndYear, projectIndex, timelines, timelineIndex = 0) {
-      if (!Array.isArray(timelines[timelineIndex])) {
-        timelines[timelineIndex] = [projectIndex]
+      endYear () {
+        return Math.max.apply(Math, this.projects.map((project) => {
+          return project.endYear
+        }))
+      },
+
+      startYear () {
+        return Math.min.apply(Math, this.projects.map((project) => {
+          return project.startYear
+        }))
+      },
+
+      timelines () {
+        let timelines = []
+
+        this.sortedProjects.forEach((project, index) => {
+          this.addToTimeline(project.endYear, index, timelines)
+        })
+
         return timelines
-      }
+      },
 
-      if (projectEndYear < this.sortedProjects[timelines[timelineIndex][timelines[timelineIndex].length -1]].startYear) {
-        timelines[timelineIndex].push(projectIndex)
-        return timelines
-      }
+      timelineYears () {
+        let timelineYears = []
 
-      this.addToTimeline(projectEndYear, projectIndex, timelines, timelineIndex + 1)
+        for (let i = this.endYear; i >= this.startYear; i--) {
+          timelineYears.push(i)
+          if (i !== this.startYear) timelineYears.push(null)
+        }
+
+        return timelineYears
+      }
     },
 
-    getProjectYears (id) {
-      return ((this.sortedProjects[id].endYear - this.sortedProjects[id].startYear) * 2) + 1
-    },
+    methods: {
+      addToTimeline (projectEndYear, projectIndex, timelines, timelineIndex = 0) {
+        if (!Array.isArray(timelines[timelineIndex])) {
+          timelines[timelineIndex] = [projectIndex]
+          return timelines
+        }
 
-    getProjectSpacing (timeline, key) {
-      if (key === 0) {
-        return (this.endYear - this.sortedProjects[timeline[key]].endYear) * 2
-      }
-      return (this.sortedProjects[timeline[key - 1]].startYear - this.sortedProjects[timeline[key]].endYear)
-    },
+        if (projectEndYear < this.sortedProjects[timelines[timelineIndex][timelines[timelineIndex].length -1]].startYear) {
+          timelines[timelineIndex].push(projectIndex)
+          return timelines
+        }
 
-    getTimelineElementStyle(projectId) {
-      let style = 'backgroundColor: ' + this.sortedProjects[projectId].color
-      if (this.selectedProject === projectId) {
-        return style + '; animationIterationCount: infinite;'
+        this.addToTimeline(projectEndYear, projectIndex, timelines, timelineIndex + 1)
+      },
+
+      getProjectYears (id) {
+        return ((this.sortedProjects[id].endYear - this.sortedProjects[id].startYear) * 2) + 1
+      },
+
+      getProjectSpacing (timeline, key) {
+        if (key === 0) {
+          return (this.endYear - this.sortedProjects[timeline[key]].endYear) * 2
+        }
+        return (this.sortedProjects[timeline[key - 1]].startYear - this.sortedProjects[timeline[key]].endYear)
+      },
+
+      getTimelineElementStyle(projectId) {
+        let style = 'backgroundColor: ' + this.sortedProjects[projectId].color
+        if (this.selectedProject === projectId) {
+          return style + '; animationIterationCount: infinite;'
+        }
+        return style
       }
-      return style
     }
   }
-}
 </script>
 
 // This should be removed, only for testing!
